74b4faad68b78c22e01a0d3ddb5b8d18b4ce96f4
Albert Yang <zhonghuaqrs@163.com>
2016-08-10 11:01:07 +0800

    add itop kernel source code

---
+++ iTop4412_Kernel_3.0/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 SMP PREEMPT Wed Nov 26 00:40:44 PST 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
